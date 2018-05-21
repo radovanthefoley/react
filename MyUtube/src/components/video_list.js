f10378c71c599e545708b99138a66e2b91da0146
Masaryk <Radovan.Masaryk@EXXETA.sk>
2018-05-21 16:28:14 +0200

    rendering arrays of react components

--- MyUtube/src/components/video_list.js
+++ MyUtube/src/components/video_list.js
@@ -1,8 +1,16 @@
 import React from 'react';
 
+import VideoListItem from './video_list_item';
+
 const VideoList = (props) => {
+  // remaping data to react components
+  const videoItems = props.videos.map((video) => <VideoListItem video={video}/>);
+
   // className is ReactJS attribute for css class
-  return <p>{props.videos.length}</p>;
+  // React correctly renders arrays of valid react components
+  return <ul className="col-md-4 list-group">
+    {videoItems}
+  </ul>;
 }
 
 export default VideoList;
