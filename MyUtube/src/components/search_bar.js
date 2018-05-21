487c5360e866def619557714504ab3c990ae4c75
Masaryk <Radovan.Masaryk@EXXETA.sk>
2018-05-21 14:43:03 +0200

    handle input change event

--- MyUtube/src/components/search_bar.js
+++ MyUtube/src/components/search_bar.js
@@ -1,7 +1,14 @@
 import React, {Component} from 'react';
 
 export default class SearchBar extends Component {
+
   render() {
-    return <input/>;
+    // onChange is react property
+    return <input onChange={this.onInputChange}/>;
+  }
+
+  // handles https://developer.mozilla.org/en-US/docs/Web/Events/change
+  onInputChange(event) {
+    console.log(event.target.value);
   }
 }
