0846ac6a2efcdd1d03156c9f953d3832791c3a4f
Masaryk <Radovan.Masaryk@EXXETA.sk>
2018-05-21 13:56:47 +0200

    adding search_bar input component

--- MyUtube/src/index.js
+++ MyUtube/src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
 const API_KEY = 'AIzaSyAc6HnYOD2eUGMVGjj1GRmREsnhaZa4Bx8';
 
 const App = () => {
   return <div>
-    Hello world!
-  </div>
+    <SearchBar/>
+  </div>;
 }
 
 ReactDOM.render(<App/>, document.querySelector('.container'));
