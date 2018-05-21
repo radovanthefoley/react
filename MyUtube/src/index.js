6f051c3690145244181f8aa49c590ba94e8bbd94
Masaryk <Radovan.Masaryk@EXXETA.sk>
2018-05-21 15:33:35 +0200

    main App component from function based to class based

--- MyUtube/src/index.js
+++ MyUtube/src/index.js
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyAc6HnYOD2eUGMVGjj1GRmREsnhaZa4Bx8';
 
-const App = () => {
-  return <div>
-    <SearchBar/>
-  </div>;
+class App extends Component {
+
+  render() {
+    return <div>
+      <SearchBar/>
+    </div>;
+  }
+
 }
 
 ReactDOM.render(<App/>, document.querySelector('.container'));
