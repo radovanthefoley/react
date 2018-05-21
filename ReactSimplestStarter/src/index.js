c84f7ee72ec7fc3c65a18a70812c539beec98199
Masaryk <Radovan.Masaryk@EXXETA.sk>
2018-05-21 13:17:30 +0200

    beautify

--- ReactSimplestStarter/src/index.js
+++ ReactSimplestStarter/src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-  return <div > Hello world! < /div>
+  return <div>
+    Hello world!
+  </div>
 }
 
-ReactDOM.render( < App / > , document.querySelector('.container'));
+ReactDOM.render(<App/>, document.querySelector('.container'));
