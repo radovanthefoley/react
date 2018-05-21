4a6fa91b1be554d91589cc2bf74485ca5553cf06
Masaryk <Radovan.Masaryk@EXXETA.sk>
2018-05-21 14:59:13 +0200

    introducing state

--- MyUtube/src/components/search_bar.js
+++ MyUtube/src/components/search_bar.js
@@ -2,10 +2,22 @@ import React, {Component} from 'react';
 
 export default class SearchBar extends Component {
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      term: ''
+    };
+  }
+
   render() {
     // onChange is react property
     // handles https://developer.mozilla.org/en-US/docs/Web/Events/change
-    return <input onChange={event => console.log(event.target.value)}/>;
+    return <div>
+      // always change state calling setState!!! reactjs magic needs to run
+      <input onChange={event => this.setState({term: event.target.value})}/>
+      Value of input: {this.state.term}
+    </div>;
   }
 
 }
