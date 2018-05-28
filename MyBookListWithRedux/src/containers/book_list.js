e92e5fc984bcc544dd2ad013b372c2447facee15
Masaryk <Radovan.Masaryk@EXXETA.sk>
2018-05-28 13:05:02 +0200

    promotion of react component to react-redux container

--- MyBookListWithRedux/src/containers/book_list.js
+++ MyBookListWithRedux/src/containers/book_list.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
 
-export default class BookList extends Component {
+class BookList extends Component {
   renderList() {
     return this
       .props
@@ -18,3 +19,11 @@ export default class BookList extends Component {
     );
   }
 }
+
+function mapStateToProps(state) {
+  // Whatever is returned will show up as props inside of BookList; props.books in
+  // this case
+  return {books: state.books};
+}
+
+export default connect(mapStateToProps)(BookList);
