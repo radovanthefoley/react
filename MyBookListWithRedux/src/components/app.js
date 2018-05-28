00dd56235df6f988d13039b5365c8ef30d02b8e6
Masaryk <Radovan.Masaryk@EXXETA.sk>
2018-05-28 13:14:23 +0200

    App rendering

--- MyBookListWithRedux/src/components/app.js
+++ MyBookListWithRedux/src/components/app.js
@@ -1,7 +1,11 @@
 import React, {Component} from 'react';
 
+import BookList from '../containers/book_list';
+
 export default class App extends Component {
   render() {
-    return (<div>React simple starter</div>);
+    return (<div>
+      <BookList/>
+    </div>);
   }
 }
