373dd9ce810ec22506cf92dcc56fca693f30ab2e
Masaryk <Radovan.Masaryk@EXXETA.sk>
2018-05-29 16:20:02 +0200

    weather reducer

--- MyWeatherForecast/src/reducers/reducer_weather.js
+++ MyWeatherForecast/src/reducers/reducer_weather.js
@@ -1,7 +1,17 @@
-export default function(state = null, action) {
+import FETCH_WEATHER from '../actions/index';
+
+export default function(state = [], action) {
   // thanks to redux-promise middleware, action is intercepted if payload key is
   // of type Promise, resolves it and sends it already resolved to reducers!
   console.log('Action received: ', action);
 
+  switch (action.type) {
+    case FETCH_WEATHER:
+      // has to be immutable! + accumulation of all the searches
+      return [
+        action.payload.data, ...state
+      ];
+      //ES6 equivalent of: return [action.payload.data].concat(state);
+  }
   return state;
 }
