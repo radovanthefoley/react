0ff7c26cd4ee2f77cbd7c8a446ec8192a437942d
Masaryk <Radovan.Masaryk@EXXETA.sk>
2018-05-30 17:10:37 +0200

    styling

--- MyWeatherForecast/src/components/google_map.js
+++ MyWeatherForecast/src/components/google_map.js
@@ -15,6 +15,6 @@ export default class GoogleMap extends Component {
 
   render() {
     // ref is for direct reference by this.refs.map
-    return <div ref="map"/>;
+    return <div className="map" ref="map"/>;
   }
 }
