6f21b14a53eb8f29cc11210e3e5d7bf4ad35bb75
Masaryk <Radovan.Masaryk@EXXETA.sk>
2018-05-30 14:11:27 +0200

    dynamic style changes

--- MyWeatherForecast/src/components/chart.js
+++ MyWeatherForecast/src/components/chart.js
@@ -6,13 +6,23 @@ function average(data) {
   return _.round(_.sum(data) / data.length);
 }
 
+// enables dynamic style changes
 export default(props) => {
+  const divStyle = {
+    textAlign: 'left',
+    fontSize: '10px',
+    margin: 0,
+    color: props.color
+  };
+
   return (
     <div>
+      <div style={divStyle}>{_.max(props.data)}</div>
       <Sparklines height={120} width={180} data={props.data}>
         <SparklinesLine color={props.color}/>
         <SparklinesReferenceLine type="avg"/>
       </Sparklines>
+      <div style={divStyle}>{_.min(props.data)}</div>
       <div>{average(props.data)}&nbsp;{props.units}</div>
     </div>
   );
