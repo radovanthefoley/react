b9ffdfc3cbd6829ce34ea38c1df4b36ae2391f2a
Masaryk <Radovan.Masaryk@EXXETA.sk>
2018-06-01 14:41:23 +0200

    combineReducers typical usage

--- ReduxSnippets/src/simple_combineReducers-impl.js
+++ ReduxSnippets/src/simple_combineReducers-impl.js
@@ -8,3 +8,47 @@ const combineReducers = (reducers) => {
       }, {});
   }
 };
+
+// ------------- -------------  TYPICAL USAGE ------------- -------------
+
+const add_reducer = (state = 0, action) => {
+  if (action.type === 'ADD') {
+    const newState = state + 1;
+    return newState;
+  }
+  return state;
+}
+
+const substract_reducer = (state = 0, action) => {
+  if (action.type === 'SUBSTRACT') {
+    const newState = state - 1;
+    return newState;
+  }
+  return state;
+}
+
+const add = {
+  type: 'ADD'
+};
+const substract = {
+  type: 'SUBSTRACT'
+};
+
+const counter = combineReducers(
+  {add: add_reducer, substract: substract_reducer}
+);
+
+const store = createStore(counter);
+/* store.getState
+[object Object] {
+  add: 0,
+  substract: 0
+}*/
+
+store.dispatch(add);
+
+/* store.getState
+[object Object] {
+  add: 1,
+  substract: 0
+}*/
