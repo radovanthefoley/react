0272360f3268ca4f5a142345815a0ede88b2b11f
rama <Radovan.Masaryk@EXXETA.sk>
2018-06-08 12:29:05 +0200

    connecting fetch_posts action + componentDidMount lifecycle callback

--- MyPostBlog/src/components/posts_index.js
+++ MyPostBlog/src/components/posts_index.js
@@ -1,9 +1,23 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
+
+import {fetchPosts} from '../actions/index';
 
 class PostsIndex extends Component {
+
+  // lifecycle hook: good place to fetch data
+  componentDidMount() {
+    this
+      .props
+      .fetchPosts();
+  }
+
   render() {
-    return <div>Posts Index</div>;
+    return <div>
+      <h3>Posts Index</h3>
+    </div>;
   }
 }
 
-export default PostsIndex;
+export default connect(null, {fetchPosts})(PostsIndex);
