fcb80b1badbe2dab9a1a03845e93be9e009f3d89
rama <Radovan.Masaryk@EXXETA.sk>
2018-06-11 15:30:27 +0200

    redux-form skeleton with one input

--- MyPostBlog/src/components/posts_new.js
+++ MyPostBlog/src/components/posts_new.js
@@ -1,11 +1,25 @@
 import React, {Component} from 'react';
+import {Field, reduxForm} from 'redux-form';
 
 class PostsNew extends Component {
-  render() {
+
+  // field attribute is send by redux-form logic for us to manually hook it
+  renderTitleField(field) {
+    // {...field.input} maps all attributes of field.input to the same keys e.g.:
+    // onBlur = field.input.onBlur
     return (<div>
-      PostsNew!
+      <input type="text" {...field.inpu}/>
     </div>);
+  }
+
+  render() {
+    return (
+      <form>
+        <Field name="title" component={this.renderTitleField}/>
+      </form>
+    );
   }
 }
 
-export default PostsNew;
+// unique name for form, in case we have multiple forms...
+export default reduxForm({form: 'PostsNewForm'})(PostsNew);
