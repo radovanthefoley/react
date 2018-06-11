6a7316ae80d9f7b661a31da70ccb90d82a58568e
rama <Radovan.Masaryk@EXXETA.sk>
2018-06-11 14:24:00 +0200

    render posts, lodash map over objects

--- MyPostBlog/src/components/posts_index.js
+++ MyPostBlog/src/components/posts_index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
@@ -13,11 +14,29 @@ class PostsIndex extends Component {
       .fetchPosts();
   }
 
+  renderPosts() {
+    // lodash can map over objects
+    return _.map(this.props.posts, post => {
+      return (
+        <li className="list-group-item" key={post.id}>
+          {post.title}
+        </li>
+      );
+    })
+  }
+
   render() {
     return <div>
-      <h3>Posts Index</h3>
+      <h3>Posts</h3>
+      <ul className="list-group">
+        {this.renderPosts()}
+      </ul>
     </div>;
   }
 }
 
-export default connect(null, {fetchPosts})(PostsIndex);
+function mapStateToProps({posts}) {
+  return {posts};
+}
+
+export default connect(mapStateToProps, {fetchPosts})(PostsIndex);
