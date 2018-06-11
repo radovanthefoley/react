ad57f7166c18f5f8116c19445688f8d7b7f8da0c
rama <Radovan.Masaryk@EXXETA.sk>
2018-06-11 16:15:18 +0200

    more on Field

--- MyPostBlog/src/components/posts_new.js
+++ MyPostBlog/src/components/posts_new.js
@@ -4,18 +4,25 @@ import {Field, reduxForm} from 'redux-form';
 class PostsNew extends Component {
 
   // field attribute is send by redux-form logic for us to manually hook it
-  renderTitleField(field) {
+  renderField(field) {
     // {...field.input} maps all attributes of field.input to the same keys e.g.:
     // onBlur = field.input.onBlur
-    return (<div>
-      <input type="text" {...field.inpu}/>
-    </div>);
+    return (
+      <div className="form-group">
+        <label>{field.label}</label>
+        <input className="form-control" type="text" {...field.inpu}/>
+      </div>
+    );
   }
 
   render() {
     return (
+      // arbitrary attributes to be passed within field, here it is label; component
+      // expects function reference that return JSX
       <form>
-        <Field name="title" component={this.renderTitleField}/>
+        <Field name="title" label="Title" component={this.renderField}/>
+        <Field name="categories" label="Categories" component={this.renderField}/>
+        <Field name="contenr" label="Post Content" component={this.renderField}/>
       </form>
     );
   }
